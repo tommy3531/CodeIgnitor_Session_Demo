65180f1fa833936b21930fec26b1a48871419045
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 17:05:51 -0600

    Added html

--- application/views/member/modelinfo.php
+++ application/views/member/modelinfo.php
@@ -1,1 +1,17 @@
-<h1> for modelinfo.php</h1>
+<div id="wrapper">
+	<div id='headerinfo'>
+		<h1>All information is taken from CodeIgniter User Guide Version 3.0</h1>
+		<a id='linktag' href ="http://www.codeigniter.com/user_guide/index.html">Codeigniter 3</a>
+	</div>
+	<div id="view_info">
+		<h1> Model Information</h1>
+		<div id='content'>
+			<p id='viewtext'>The model is used to interact with your database, I autoloaded the database in the config/autloader, so I
+							 could user $this->db which made inserting data very easy.  If you would like to learn please click the below link
+			</p>
+			<p id="learning link">
+				<a href="http://www.codeigniter.com/user_guide/general/controllers.html?highlight=controller">Learn about Model</a>
+			</p>
+		</div>
+	</div>
+</div>
