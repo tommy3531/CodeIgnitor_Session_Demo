93acef6ee28bb2c0d59a87eb1a962d919c23a931
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 17:58:27 -0600

    Added image html

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,4 +1,9 @@
-<!-- Put JS here -->
-<h1> Footer View</h1>
-</body>
-</html>
+
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Tom Marler 2015</p>
+        </div>
+    </div>
+</footer>
