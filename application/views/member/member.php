6448891b8e4f7832405de4d5155ce37c646b8570
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 17:37:44 -0600

    Added html

--- application/views/member/member.php
+++ application/views/member/member.php
@@ -2,3 +2,7 @@
 	<div class="alert alert-success"> <?php echo $this->session->flashdata('loggin_success') ?> </div>
 <?php } ?>
 
+<div class="jumbotron">
+  <h1>Hello, Members</h1>
+  <p>Please click on the about links to learn about Codeigniter</p>
+</div>
