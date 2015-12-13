424be960306a6b953ddbad982ae106323ca87eb5
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 16:37:35 -0600

    Added html content

--- application/views/member/viewinfo.php
+++ application/views/member/viewinfo.php
@@ -1,1 +1,18 @@
-<h1> for viewinfo.php</h1>
+<h1>All information is taken from CodeIgniter User Guide Version 3.0</h1>
+<a href ="http://www.codeigniter.com/user_guide/index.html">Codeigniter 3</a>
+
+<div id="view_info">
+	<h1> View information</h1>
+	<div id='view_content'>
+		<p id='view_text'>Views are located in the application/view director.
+			              they receive information from there respective controller on what to show the user.
+			              for example if I had a controller named login I would have a function called index.
+			              The index function in the controller login would load the login form, whenever a user,
+			              would click the login button the page.
+		</p>
+		<p id="learning link">Learn about view<a href="http://www.codeigniter.com/user_guide/general/views.html?highlight=views"></a></p>
+
+
+	</div>
+
+</div>
