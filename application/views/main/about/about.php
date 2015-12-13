60a4292c65792863cebc7bbaf79d06863dea9205
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 15:36:44 -0600

    Added content to about me page

--- application/views/main/about/about.php
+++ application/views/main/about/about.php
@@ -2,5 +2,14 @@
 <!DOCTYPE>
 <html>
 
-<h1>Syrian Refugee criss</h1>
+<div class="jumbotron">
+  <h1>This site is dedicated to learning about codeigniter and deploying to azure</h1>
+  <p>- You will learn about the codeigniter structure</p>
+  <p>- You will learn aboout views and passing data. </p>
+  <p>- You will learn about deploying a codeigniter project to azure</p>
+  <p>- You will learn about how to configure your application/config file for a database</p>
+  <p>- You will learn about how to autoload libraries</p>
+  <p>- Last but not least learn you might learn something new!</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
 
