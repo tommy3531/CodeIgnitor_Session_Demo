86d11b9df32feda69b2da29d34c479f79756a62f
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 16:10:48 -0600

    Added test function to members link

--- application/views/member/top_nav_member.php
+++ application/views/member/top_nav_member.php
@@ -3,24 +3,25 @@
 	<nav id="mainnavbar" class="navbar navbar-default navbar-fixed-top"> <!-- Start #mainnavbar -->
 		<div class="container"> <!-- Start of Container -->
 			<div class="navbar-header"> <!-- Start div.navhar-header -->
-		        <a class="navbar-brand" href="?php echo site_url('home'); ?>">Missouri Politics</a>
+		        <a class="navbar-brand" href="?php echo site_url('home'); ?>">Learn Codeigniter</a>
 			</div> <!-- End div.navbar-header -->
 			<div id="topnavbar" class="navbar-collapse collapse"> <!-- Start #topnavbar -->
 				<ul id ="leftnav" class="nav navbar-nav"> <!-- Start #leftnav -->
 					<li class="active">
-						<a href="<?php echo site_url('home'); ?>">Home</a>
+						<a href="<?php echo site_url('members/viewinfo'); ?>">Views</a>
 					</li>
 					<li>
-						<a href="<?php echo site_url('member/info'); ?>">Member area</a>
+						<a href="<?php echo site_url('member/controllerinfo'); ?>">Controller Info</a>
 					</li>
 					<li>
-						<a href="<?php echo site_url('member/'); ?>">Members Area</a>
+						<a href="<?php echo site_url('member/modelinfo'); ?>">Model Info</a>
+					</li>
+					<li>
+						<a href="<?php echo site_url('member/azureinfo'); ?>">Azure Info</a>
 					</li>
 				</ul> <!-- End #leftnav -->
 				<ul id="rightnav" class="nav navbar-nav navbar-right"> <!-- Start #rightnav -->
-					<li>
-						<a href="<?php echo site_url('member/account'); ?>">Account</a>
-					</li>
+
 					<li class="active">
 						<a href="<?php echo site_url('member/logout'); ?>">Logout</a>
 						<span class="sr-only">(Current)</span>
