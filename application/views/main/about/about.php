dbbc2f399d48014d97c5a6792334ff08bfd788c3
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 15:54:10 -0600

    Used CSS to style contact page

--- application/views/main/about/about.php
+++ application/views/main/about/about.php
@@ -20,14 +20,16 @@
 
 <div class="jumbotron">
 	<div id='title'>
-  		<h1>This site is dedicated to learning about codeigniter and deploying to azure</h1>
+  		<h1>Dedicated to learning about codeigniter and deploying to azure</h1>
   	</div>
   <p>- You will learn about the codeigniter structure</p>
   <p>- You will learn aboout views and passing data. </p>
   <p>- You will learn about deploying a codeigniter project to azure</p>
   <p>- You will learn about how to configure your application/config file for a database</p>
   <p>- You will learn about how to autoload libraries</p>
-  <p>- Last but not least learn you might learn something new!</p>
-  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+  <p>- Last but not least you might learn something new!</p>
+  <p>- Become a member and get access to a free youtube free!</p>  
+
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a></p>
 </div>
 
