1ba7a1e119af61d3699f9b88fef8d2fd4d526f82
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 16:59:42 -0600

    html content

--- application/views/member/controllerinfo.php
+++ application/views/member/controllerinfo.php
@@ -1,1 +1,25 @@
-<h1> for controllerinfo.php</h1>
+<div id="wrapper">
+	<div id='headerinfo'>
+		<h1>All information is taken from CodeIgniter User Guide Version 3.0</h1>
+		<a id='linktag' href ="http://www.codeigniter.com/user_guide/index.html">Codeigniter 3</a>
+	</div>
+	<div id="view_info">
+		<h1> Controller Information</h1>
+		<div id='content'>
+			<p id='viewtext'>Controllers are considered the middle man when talking about views and models. On mysite the model 
+							 communicates with the Database when a user logins to verify there password.  If you the passwords match
+							 the model sends the user_id to the controller.  The controller uses the user_id in a session to see if the 
+							 user is logged in or not.  If the user presses the logout button the session is destory and the controller
+							 tells the view to redirect the user to the home page.
+
+							 Controllers are great for doing form validation becasue the controller can send flashdata to view
+							 (which is an error message) to display form errors to the user.  If you plan on using flashdata make sure
+							 $this-load->view('controller/method'), becasue error messages go away on redirect.  You can find more information
+							 about controller below.
+			</p>
+			<p id="learning link">
+				<a href="http://www.codeigniter.com/user_guide/general/controllers.html?highlight=controller">Learn about Controller</a>
+			</p>
+		</div>
+	</div>
+</div>
