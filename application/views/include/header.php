96a48e9b374b0fb205b623ccfa192b169bb65e71
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 18:59:55 -0600

    Added js

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" >
 	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-	<script src="jquery.elevatezoom.js" type="text/javascript"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.elevatezoom.js"></script>
 	
 	<!-- Load custom css and js files to overwrite boostrap if neccesary -->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/custom.css" >
