667171da880171d5d940af859927520258998ab7
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 18:47:30 -0600

    Changed image size

--- application/views/member/pictureinfo.php
+++ application/views/member/pictureinfo.php
@@ -10,22 +10,22 @@
 
         <div class="col-lg-3 col-md-4 col-xs-6 thumb">
             <a class="thumbnail" href="#">
-                <img id="azureimg" src="http://testsitetom.azurewebsites.net/member/pictureinfo/images/azuregit.png" alt="gitpicture" style="height:400px;width:300px">
+                <img id="azureimg" src="<?php echo base_url(); ?>assets/images/azuregit.png" alt="gitpicture" style="height:300px;width:500px">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb">
             <a class="thumbnail" href="#">
-                <img id="databaseimg" src="<?php echo base_url(); ?>assets/images/database.png" alt="database">
+                <img id="databaseimg" src="<?php echo base_url(); ?>assets/images/database.png" alt="database" style="height:300px;width:500px">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb">
             <a class="thumbnail" href="#">
-                <img id="gitrepoimg" src="<?php echo base_url(); ?>assets/images/gitrepo.png" alt="gitrepo">
+                <img id="gitrepoimg" src="<?php echo base_url(); ?>assets/images/gitrepo.png" alt="gitrepo" style="height:300px;width:500px">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb">
             <a class="thumbnail" href="#">
-                <img id="img-responsive" src"<?php echo base_url(); ?>assets/images/session.png" alt="session">
+                <img id="img-responsive" src"<?php echo base_url(); ?>assets/images/session.png" alt="session" style="height:300px;width:500px">
             </a>
         </div>
     </div>
