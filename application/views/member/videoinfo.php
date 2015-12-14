04148c5647dd649508d3938c1c80d6224d82cf39
Tom Marler <thomas.marler@hotmail.com>
2015-12-13 19:21:11 -0600

    Added css

--- application/views/member/videoinfo.php
+++ application/views/member/videoinfo.php
@@ -1,5 +1,10 @@
-<div class='wrapper'>
-	<div id='video'>
-		<iframe width="20" height="315" src="https://www.youtube.com/embed/bBb_Hi2Odqc" frameborder="0" allowfullscreen></iframe>
-	</div>
+<div class="container">See how to deploy codeigniter to azure</div>
+
+<div class="container">
+    
+    <div class="videocontainer">
+       <iframe width="20" height="315" src="https://www.youtube.com/embed/bBb_Hi2Odqc" frameborder="0" allowfullscreen></iframe>
+    </div>
+    
 </div>
+
